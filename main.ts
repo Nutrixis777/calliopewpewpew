feea9b8aa484eca4d5a049b1432cc4beb798268a
Nutrixis777 <82807257+Nutrixis777@users.noreply.github.com>
2021-05-19 09:56:34 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,7 +6,6 @@ input.onButtonPressed(Button.A, function () {
         . . . # .
         . . # . .
         `)
-    radio.setGroup(10)
     basic.pause(100)
     for (let index = 0; index < 1; index++) {
         radio.sendString("nx7")
@@ -24,6 +23,19 @@ radio.onReceivedString(function (nx7) {
         . . . . .
         `)
     basic.showString("nx7")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    basic.pause(100)
+    for (let index = 0; index < 1; index++) {
+        radio.sendString("Sheesh")
+    }
 })
 basic.forever(function () {
     radio.setGroup(10)
